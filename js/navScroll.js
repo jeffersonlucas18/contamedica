2670d154cb4a1c9f10ca5be68aecaac2ffddadb6
jefferson lucas <jefferson.lucas114@gmail.com>
2018-10-25 16:10:51 -0300

    site compactadas :html,css e js

--- js/navScroll.js
+++ js/navScroll.js
@@ -1,13 +1,1 @@
-function scrollIntoView(link) {   
-    document.querySelector(link).scrollIntoView({
-        behavior: 'smooth',
-    })
-}
-(function(){
-    $("[data-goto]").click(function(){
-        var selector = $(this).attr("data-goto");
-        console.log("Scrolling to:", selector);
-            
-        scrollIntoView(selector);                        
-    });                   
-})();
+function scrollIntoView(link){document.querySelector(link).scrollIntoView({behavior:'smooth',})}(function(){$("[data-goto]").click(function(){var selector=$(this).attr("data-goto");console.log("Scrolling to:",selector);scrollIntoView(selector)})})()
